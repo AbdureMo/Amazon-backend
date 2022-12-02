9baf1f652bca167fb966e476215ac2a418a7208a
AbdureMo <abdurehmanmohammed45@gmail.com>
2022-12-02 15:39:07 +0300

    changed

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 3000;
 
-const stripe = require("stripe")(
-  "sk_test_51Lz3XlBtfcWg7X9gQRkbXQlNCvoikUt021IF0fJKofr9iivGMxYxkAF90wPIqW2AtYBiZhrZH3mxdOXi2wSO3Jpe00nnEZufUI"
-);
+// const stripe = require("stripe")(
+//   "sk_test_51Lz3XlBtfcWg7X9gQRkbXQlNCvoikUt021IF0fJKofr9iivGMxYxkAF90wPIqW2AtYBiZhrZH3mxdOXi2wSO3Jpe00nnEZufUI"
+// );
 
 // - App config
 const app = express();
@@ -17,21 +17,21 @@ app.get("/", (request, response) => response.status(200).send("hello world"));
 app.get("/home", (request, response) =>
   response.status(200).send("Abdure the coder")
 );
-app.post("/payments/create", async (request, response) => {
-  const total = request.query.total;
+// app.post("/payments/create", async (request, response) => {
+//   const total = request.query.total;
 
-  console.log("Payment Request Recieved for this amount >>> ", total);
+//   console.log("Payment Request Recieved for this amount >>> ", total);
 
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: total, // subunits of the currency
-    currency: "usd",
-  });
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: total, // subunits of the currency
+//     currency: "usd",
+//   });
 
-  // OK - Created
-  response.status(201).send({
-    clientSecret: paymentIntent.client_secret,
-  });
-});
+//   // OK - Created
+//   response.status(201).send({
+//     clientSecret: paymentIntent.client_secret,
+//   });
+// });
 
 app.listen(PORT, () => {
   console.log(`serever running on port: ${PORT}`);
